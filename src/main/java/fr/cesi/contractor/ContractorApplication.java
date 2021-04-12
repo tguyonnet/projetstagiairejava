57e7956628875672b97c0268d03d423736dfe04a
Thomas <thomas.beuch@gmail.com>
2021-04-12 16:04:49 +0200

    new project

---
+++ src/main/java/fr/cesi/contractor/ContractorApplication.java
@@ -0,0 +1,13 @@
+package fr.cesi.contractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractorApplication.class, args);
+    }
+
+}
