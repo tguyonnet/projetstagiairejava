5f0c94bc98fe950bee6a1481191ec050bae704db
Kevin <kevin/nave@viacesi.fr>
2021-04-13 11:20:26 +0200

    :hammer: Changed route to /project

--- src/main/java/fr/cesi/contractor/Controllers/ProjectController.java
+++ src/main/java/fr/cesi/contractor/Controllers/ProjectController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/projects")
+@RequestMapping("/api/project")
 public class ProjectController {
 
     @Autowired
