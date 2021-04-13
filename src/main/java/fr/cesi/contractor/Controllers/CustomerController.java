f112598d4af50b3efa075badedc2b7e62e1722b0
Thomas <thomas.beuch@gmail.com>
2021-04-13 16:45:21 +0200

    routes,meme champs,  loaddatabase a jour

--- src/main/java/fr/cesi/contractor/Controllers/CustomerController.java
+++ src/main/java/fr/cesi/contractor/Controllers/CustomerController.java
@@ -23,7 +23,7 @@ public class CustomerController {
      *
      * @return the list
      */
-    @GetMapping("/index")
+    @GetMapping("/customers")
     public List<Customer> getAllCustomers()
     {
         return customerRepository.findAll();
@@ -49,7 +49,7 @@ public class CustomerController {
      * @return
      * @throws ResourceNotFoundException
      */
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<Customer> updateCustomer(@PathVariable(value = "id") Integer customerId, @Validated @RequestBody Customer customerDetail) throws ResourceNotFoundException
     {
         Customer customer = customerRepository.findById(customerId)
@@ -70,7 +70,7 @@ public class CustomerController {
      * @param customerID
      * @return
      */
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteCustomer(@PathVariable(value = "id") Integer customerID)
     {
         Customer customer = customerRepository.findById(customerID)
