2d0a61ff09bf61c2440cd3e5e2d38a7a7d106b5f
Clem <clement.savary@viacesi.fr>
2021-04-16 09:41:23 +0200

    :zap: Add CrossOrigin

--- src/main/java/fr/cesi/contractor/Controllers/ItemController.java
+++ src/main/java/fr/cesi/contractor/Controllers/ItemController.java
@@ -3,6 +3,7 @@ package fr.cesi.contractor.Controllers;
 import fr.cesi.contractor.Models.Item;
 import fr.cesi.contractor.Repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api")
 public class ItemController {
     @Autowired
